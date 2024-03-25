2940dee6f2cf368abe5d389dae75da0f0394d68a
Apurva Mahajan <135137373+amahaja25@users.noreply.github.com>
2024-03-25 19:14:31 +0000

    update app.py

--- app.py
+++ app.py
@@ -18,23 +18,29 @@ class Notice(Model):
         table_name = "notices"
         database = db
 
+class ZipCode(Model):
+    zipcode = CharField()
+    owner_occupied = IntegerField(null = True)
+
+    class Meta:
+        database = db
+
 @app.route("/")
 def index():
     notice_count = Notice.select().count()
-    all_zips = (Notice.select(Notice.zip).distinct())
+    all_zips = ZipCode.select()
     template = 'index.html'
     return render_template(template, count = notice_count, all_zips = all_zips)
 
 @app.route('/zipcode/<slug>')
 def detail(slug):
-    zipcode = slug
+    zipcode = ZipCode.get(ZipCode.zipcode==slug)
     notices = Notice.select().where(Notice.zip==slug)
     total_notices = Notice.select(fn.SUM(Notice.notices).alias('sum')).where(Notice.zip==slug).scalar()
     notice_json = []
     for notice in notices:
         notice_json.append({'x': str(notice.month.year) + ' ' + str(notice.month.month), 'y': notice.zip, 'heat': notice.notices})
     owner_occupied = c.acs5.state_zipcode(('NAME', 'B25003_002E'), '24', zipcode)
-    return render_template("detail.html", zipcode=zipcode, notices=notices, notices_count=len(notices), notice_json = notice_json, total_notices = total_notices, owner_occupied = int(owner_occupied[0]['B25003_002E']))
-
+    return render_template("detail.html", zipcode=zipcode, notices=notices, notices_count=len(notices), notice_json = notice_json, total_notices = total_notices)
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
